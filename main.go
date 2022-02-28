0c1748f67d5e8175ccc4dc9cc28bd51d273a17d4
Valdeci Gomes <valfilho@amazon.com>
2022-02-28 19:56:59 -0300

    Add example of how save to file

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	fmt.Println("----")
-	remainingDeck.print()
+	//hand, remainingDeck := deal(cards, 5)
+	//hand.print()
+	//fmt.Println("----")
+	//remainingDeck.print()
+
+	cards.saveToFile("my_card.txt")
 }
 
 //array => fixed length list of thing
 //slice => list with length unfixed
+
+// convert string to byte example
+//greeting := "Hi there"
+//byteString := []byte(greeting)

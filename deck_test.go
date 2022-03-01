0886c40d7c31e2586be2e8be67994256a6b5bb26
Valdeci Gomes <valfilho@amazon.com>
2022-02-28 22:13:09 -0300

    Add io testing

--- deck_test.go
+++ deck_test.go
@@ -1,19 +1,41 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+var numberOfCards = 16
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
+	deckSize := len(d)
 
-	if len(d) != 16 {
-		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	if deckSize != numberOfCards {
+		t.Errorf("Expected deck length of %v, but got %v", numberOfCards, deckSize)
 	}
 
 	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
 
-	if d[len(d)-1] != "Four of Clubs" {
-		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	if d[deckSize-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[deckSize-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting.txt"
+	os.Remove(fileName)
+	deck := newDeck()
+	deck.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+	loadedDeckSize := len(loadedDeck)
+
+	if loadedDeckSize != numberOfCards {
+		t.Errorf("Expected %v cards in deck, got %v", numberOfCards, loadedDeckSize)
+	}
+
+	os.Remove(fileName)
 }
